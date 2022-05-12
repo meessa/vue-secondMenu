eeb00920ed7a0eff432d01c981693b11e559e993
Your Name <you@example.com>
2022-05-12 18:32:29 +0800

    初始化

---
+++ src/page/test2/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-aside>
+      <SideMenu :itemList='itemList' />
+    </el-aside>
+    <el-main>
+      <router-view />
+    </el-main>
+  </div>
+</template>
+
+<script>
+import SideMenu from '@/components/sidemenu/SideMenu'
+export default {
+  components: {
+    SideMenu
+  },
+  data() {
+    return {
+      itemList: [
+        { path: 'test2-1', title: '二级菜单2-1' },
+        { path: 'test2-2', title: '二级菜单2-2' },
+        { path: 'test2-3', title: '二级菜单2-3' }
+      ]
+    }
+  },
+}
+</script>
---
+++ src/page/test3/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-aside>
+      <SideMenu :itemList="itemList" />
+    </el-aside>
+    <el-main>
+      <router-view />
+    </el-main>
+  </div>
+</template>
+
+<script>
+import SideMenu from '@/components/sidemenu/SideMenu'
+export default {
+  components: {
+    SideMenu
+  },
+  data() {
+    return {
+      itemList: [
+        { path: 'test3-1', title: '二级菜单3-1' },
+        { path: 'test3-2', title: '二级菜单3-2' }
+      ]
+    }
+  }
+}
+</script>
